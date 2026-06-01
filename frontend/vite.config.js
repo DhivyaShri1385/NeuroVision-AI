74d31ff7b85ae7a8b926fe579315b684ab9e159a
DHIVYA SHRI S <167316138+DhivyaShri1385@users.noreply.github.com>
2026-06-01 09:43:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"
+NeuroVision-AI
+",
   plugins: [react()],
   server: {
     port: 3000,
